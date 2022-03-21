47cff2733ee8f7c91acbc4d804bcd7e2c496792b
Zeyao Li <zeyaoli@Zeyaos-MBP.lan>
2022-03-21 00:36:26 -0400

    style update!

--- src/components/Match.js
+++ src/components/Match.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./Match.css";
 
 const Match = ({ isMatched, setIsMatched, person }) => {
-  const handleClick = () => {
-    setIsMatched(false);
-  };
+    const handleClick = () => {
+        setIsMatched(false);
+    };
 
-  return (
-    <div className='match' style={{ display: isMatched ? "flex" : "none" }}>
-      <h3>So we matched... but what happened after?</h3>
-      <p className='match__text'>{person.story}</p>
-      <button onClick={handleClick}>thank u, next</button>
-    </div>
-  );
+    return (
+        <div className="match" style={{ display: isMatched ? "flex" : "none" }}>
+            <h3>So we matched... but what happened after?</h3>
+            <p className="match__text">{person.story}</p>
+            <button onClick={handleClick}>Boy bye</button>
+        </div>
+    );
 };
 
 export default Match;
